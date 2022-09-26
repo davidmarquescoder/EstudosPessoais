28859afb2200452b811d21ffa6a11967968d67f4
David Marques <104264104+davidmarquescoder@users.noreply.github.com>
2022-09-25 23:18:46 -0300

    Inicio do meu aprendizado JAVA
    Minhas primeiras linhas em JAVA

---
+++ JAVA/ProjectOne/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static  void main(String[] args){
+        var login= "Olá mundo"; //Para criar uma variável em JAVA deve-se utilizar "var" antes do nome da variável
+        int num = 10;          //Definindo o tipo primitivo da variável
+        double num1 = 10.5;   //No java não se usa Float, mas usa-se o Double
+        boolean num2  = true;
+
+        System.out.println(login); //print em java
+        System.out.println(num);
+        System.out.println(num1);
+        System.out.println(num2);
+    }
+}
