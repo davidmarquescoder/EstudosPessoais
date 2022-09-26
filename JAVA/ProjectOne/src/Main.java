187af99e08e7a916076d1a32a98661b271555005
David Marques <104264104+davidmarquescoder@users.noreply.github.com>
2022-09-25 23:47:03 -0300

    Update 25_09_2022

--- JAVA/ProjectOne/src/Main.java
+++ JAVA/ProjectOne/src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static  void main(String[] args){
-        var login= "Olá mundo"; //Para criar uma variável em JAVA deve-se utilizar "var" antes do nome da variável
+
+        var login= "Olá mundo"; //Para criar uma variável em JAVA deve-se utilizar "var" antes do nome da variável ou o tipo primitivo
         int num = 10;          //Definindo o tipo primitivo da variável
         double num1 = 10.5;   //No java não se usa Float, mas usa-se o Double
         boolean num2  = true;
