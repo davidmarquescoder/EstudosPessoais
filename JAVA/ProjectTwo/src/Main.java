595378d111c941e3615420c74e87222f594e972b
David Marques <104264104+davidmarquescoder@users.noreply.github.com>
2022-09-27 15:35:04 -0300

    Novo Projeto
    Tratando de argumentos e parâmetros

---
+++ JAVA/ProjectTwo/src/Main.java
@@ -0,0 +1,8 @@
+//Nesse projeto vamos estudar um pouco sobre parametros e argumentos, foi visto no projeto passado
+//Porém esse vai ser dedicado somente  a esses assuntos.
+
+public class Main {
+    public static void main(String[] args){ //Esse "String[]" serve como uma coleção ou lista, posso armazenar valores dentro dele e o "args" é serve como uma variavel o string é o tipo primitivo
+        System.out.println(args[0]); //Para colocar algum valor dentro dos indices eu informo naa hora de ccompilar o projeto pelo CMD
+    }//Para compilar aqui pela IDE tem que ccolocar os argumentos no projeto (não sei como faz isso)
+}
