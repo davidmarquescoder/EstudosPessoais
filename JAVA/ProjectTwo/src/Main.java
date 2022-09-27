6e45e6575ea101de1d7f724de306e90e194d1082
David Marques <104264104+davidmarquescoder@users.noreply.github.com>
2022-09-27 15:43:39 -0300

    Update

--- JAVA/ProjectTwo/src/Main.java
+++ JAVA/ProjectTwo/src/Main.java
@@ -3,6 +3,8 @@
 
 public class Main {
     public static void main(String[] args){ //Esse "String[]" serve como uma coleção ou lista, posso armazenar valores dentro dele e o "args" é serve como uma variavel o string é o tipo primitivo
-        System.out.println(args[0]); //Para colocar algum valor dentro dos indices eu informo naa hora de ccompilar o projeto pelo CMD
-    }//Para compilar aqui pela IDE tem que ccolocar os argumentos no projeto (não sei como faz isso)
+        System.out.println("Primeiro nome: "+args[0]); //Para colocar algum valor dentro dos indices eu informo naa hora de compilar o projeto pelo CMD
+        System.out.println("Segundo nome: "+args[1]);
+        System.out.println("Terceiro nome: "+args[2]);
+    }//Para compilar aqui pela IDE, tem que colocar os argumentos nas propriedades do projeto (não sei como faz isso)
 }
