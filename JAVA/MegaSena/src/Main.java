0ff41f30709e4157efceb03655351f164321e410
David Marques <104264104+davidmarquescoder@users.noreply.github.com>
2022-09-30 15:33:32 -0300

    Gerando números aleatórios

---
+++ JAVA/MegaSena/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        int i = 0;
+        Random generate = new Random();
+
+        while(i<=9){
+            int number = generate.nextInt(60);
+            System.out.println(number);
+            i++;
+        }
+    }
+}
