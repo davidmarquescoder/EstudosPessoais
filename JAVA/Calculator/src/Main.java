8a57aa1c86495be897d287d6228d73be516df367
David Marques <104264104+davidmarquescoder@users.noreply.github.com>
2022-09-30 15:09:26 -0300

    .class att

--- JAVA/Calculator/src/Main.java
+++ JAVA/Calculator/src/Main.java
@@ -1,12 +1,33 @@
 public class Main {
     public static void main(String[] args){
-        int x = Integer.parseInt(args[1]);
+        int x = Integer.parseInt(args[0]);
         int y = Integer.parseInt(args[2]);
-        if(args[0].equals("somar")){
+
+        if(args[1].equals("+")){
             soma(x, y);
         }
+        if(args[1].equals("-")){
+            sub(x, y);
+        }
+        if(args[1].equals("#")){
+            mult(x, y);
+        }
+        if(args[1].equals("/")){
+            divs(x, y);
+        }
     }
+
+    //Funções ou Métodos
     static void soma(int x, int y){
         System.out.println(x+y);
+    }
+    static void sub(int x, int y){
+        System.out.println(x-y);
+    }
+    static void mult(int x, int y){
+        System.out.println(x*y);
+    }
+    static void divs(int x, int y){
+        System.out.println(x/y);
     }
 }
